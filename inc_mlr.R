901613450528b9c6eaf6dc10dcb1d4f35cc06e37
jhart6 <jhart6@wisc.edu>
2016-10-31 12:24:32 -0500

    reverse the order of the MLR to be co2 THEN ch4

--- inc_mlr.R
+++ inc_mlr.R
@@ -3,6 +3,7 @@ setwd("~/Desktop/Alaska Project/JH Stats Analysis/Bidirectional Plots/")
 inc<-read.csv("incubation.csv")
 View(inc)
 
+####CH4 + CO2####
 mlr<-lm(inc$avg_diff13C~inc$avg_ch4+inc$avg_co2)
 summary(mlr)
 resid(mlr)
@@ -11,7 +12,19 @@ shapiro.test(resid(mlr)) #normally distributed model residuals
 mlr$coefficients
 preds<-c(predict(mlr))
 
+####CO2 + CH4####
+mlr<-lm(inc$avg_diff13C~inc$avg_co2+inc$avg_ch4)
+summary(mlr)
+resid(mlr)
+shapiro.test(resid(mlr)) #normally distributed model residuals
+
+mlr$coefficients
+preds<-c(predict(mlr))
+
+
 quartz()
 par(mar=c(5,5,2,1))
 plot(preds,inc$avg_diff13C,pch=16,cex=1.5,xlab=expression("Observed"),ylab=expression("Modeled"))
 abline(0,1,col='red')
+mod<-lm(inc$avg_diff13C~preds)
+summary(mod)
