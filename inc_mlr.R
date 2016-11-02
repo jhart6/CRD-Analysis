a4cdfae05d9a7002aefa7fd8838d57ce781ff14c
jhart6 <jhart6@wisc.edu>
2016-11-02 14:16:41 -0500

    making sure CO2 and CH4 production weren't correlated at the incubation scale

--- inc_mlr.R
+++ inc_mlr.R
@@ -3,6 +3,12 @@ setwd("~/Desktop/Alaska Project/JH Stats Analysis/Bidirectional Plots/")
 inc<-read.csv("incubation.csv")
 View(inc)
 
+cor.test(inc$avg_ch4,inc$avg_co2)
+plot(inc$avg_ch4,inc$avg_co2)
+mod<-lm(inc$avg_co2~inc$avg_ch4)
+abline(mod)
+
+
 ####CH4 + CO2####
 mlr<-lm(inc$avg_diff13C~inc$avg_ch4+inc$avg_co2)
 summary(mlr)
@@ -24,7 +30,7 @@ preds<-c(predict(mlr))
 
 quartz()
 par(mar=c(5,5,2,1))
-plot(preds,inc$avg_diff13C,pch=16,cex=1.5,xlab=expression("Observed"),ylab=expression("Modeled"))
+plot(preds,inc$avg_diff13C,pch=16,xlab=NA,ylab=NA,cex=1.5,cex.lab=1.5,cex.axis=1.5,ylim=c(-2.0,0),xlim=c(-2.0,0)) #x = modeled, y = observed
 abline(0,1,col='red')
 mod<-lm(inc$avg_diff13C~preds)
 summary(mod)
