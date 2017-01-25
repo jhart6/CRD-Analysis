e03e95f0cad5cac1342412f9071a00e2f65c1b95
jhart6 <jhart6@wisc.edu>
2017-01-25 14:05:42 -0600

    changed red 1:1 line to dashed b/w

--- inc_mlr.R
+++ inc_mlr.R
@@ -31,6 +31,6 @@ preds<-c(predict(mlr))
 quartz()
 par(mar=c(5,5,2,1))
 plot(preds,inc$avg_diff13C,pch=16,xlab=NA,ylab=NA,cex=1.5,cex.lab=1.5,cex.axis=1.5,ylim=c(-2.0,0),xlim=c(-2.0,0)) #x = modeled, y = observed
-abline(0,1,col='red')
+abline(0,1,lty=5,lwd=2)
 mod<-lm(inc$avg_diff13C~preds)
 summary(mod)
